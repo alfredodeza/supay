a1ee7fbd898f96a246d020ee18f9c47b8d5a0c07
Alfredo Deza <Alfredo.Deza@coxinc.com>
2011-09-07 13:26:26 -0400

    removed bad mock file.

--- tests/test_supay.py
+++ tests/test_supay.py
@@ -5,8 +5,19 @@ from subprocess import Popen, PIPE
 from time import sleep
 
 import supay
-import test_script
-from mock import MockSys
+
+class MockSys(object):
+    """Can grab messages sent to stdout or stderr"""
+    def __init__(self):
+        self.message = []
+
+    def write(self, string):
+        self.message.append(string)
+        pass
+                                                                                                                                                                                                         
+    def captured(self):
+        return ''.join(self.message)
+
 
 class TestDaemon(unittest.TestCase):
     
